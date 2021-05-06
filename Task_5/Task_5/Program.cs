4c5fc424fb3daaa7fa2c0357e348fcfaf294fa83
screentolik <miroslav.daniliyk@gmail.com>
2021-05-06 12:25:56 +0300

    Змінено з idE https://github.com/Luciferochka/TCPP-Daniliyk-MainProject/issues/1

--- Task_5/Task_5/Program.cs
+++ Task_5/Task_5/Program.cs
@@ -9,7 +9,7 @@ namespace Task_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from IDE");
         }
     }
 }
